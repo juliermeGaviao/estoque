124eda1ac8e4e65fbe8dcd2eeb9c1f1b28cb2706
julierme.gaviao <julierme.gaviao@jointecnologia.com.br>
2025-08-28 18:31:30 -0300

    Remoção de mais arquivos do framework Sakai.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -33,25 +33,9 @@ const router = createRouter({
             ]
         },
         {
-            path: '/pages/notfound',
-            name: 'notfound',
-            component: () => import('@/views/pages/NotFound.vue')
-        },
-
-        {
             path: '/auth/login',
             name: 'login',
             component: () => import('@/views/pages/auth/Login.vue')
-        },
-        {
-            path: '/auth/access',
-            name: 'accessDenied',
-            component: () => import('@/views/pages/auth/Access.vue')
-        },
-        {
-            path: '/auth/error',
-            name: 'error',
-            component: () => import('@/views/pages/auth/Error.vue')
         }
     ]
 })
