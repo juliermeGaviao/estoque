e3b3193348d76b3fd208b73df5fd9ff4ba0252a5
julierme.gaviao <julierme.gaviao@jointecnologia.com.br>
2025-09-12 20:44:10 -0300

    Incluídas das tabelas de preços.

---
+++ backend/src/main/java/br/com/dinamica/estoque/entity/TabelaPreco.java
@@ -0,0 +1,35 @@
+package br.com.dinamica.estoque.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tabela_preco")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TabelaPreco {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_alteracao", nullable = false)
+    private Date dataAlteracao;
+
+}
---
+++ backend/src/main/java/br/com/dinamica/estoque/entity/TabelaPrecoProduto.java
@@ -0,0 +1,47 @@
+package br.com.dinamica.estoque.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tabela_preco_produto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TabelaPrecoProduto {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_produto", nullable = false)
+    private Produto produto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_tabela_preco", nullable = false)
+    private TabelaPreco tabela;
+
+    @Column(name = "preco", nullable = false, precision = 19, scale = 2, columnDefinition = "decimal")
+    private BigDecimal preco;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_alteracao", nullable = false)
+    private Date dataAlteracao;
+
+}
