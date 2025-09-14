d53e8b1272b2cd62eaf13eadd41e6aab5000c3f9
julierme.gaviao <julierme.gaviao@jointecnologia.com.br>
2025-09-14 19:55:19 -0300

    Conclusão da definição das tabelas do sistema.

---
+++ backend/src/main/java/br/com/dinamica/estoque/entity/UsuarioTabelaPreco.java
@@ -0,0 +1,43 @@
+package br.com.dinamica.estoque.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario_tabela_preco")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioTabelaPreco {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_tabela_preco", nullable = false)
+    private TabelaPreco tabela;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_alteracao", nullable = false)
+    private Date dataAlteracao;
+
+}
---
+++ backend/src/main/java/br/com/dinamica/estoque/entity/Venda.java
@@ -0,0 +1,49 @@
+package br.com.dinamica.estoque.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "venda")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Venda {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_tabela_preco_produto", nullable = false)
+    private TabelaPrecoProduto tabelaPrecoProduto;
+
+    @Column(name = "quantidade", nullable = false)
+    private Integer quantidade;
+
+    @Column(name = "desconto", nullable = false)
+    private Float desconto;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_alteracao", nullable = false)
+    private Date dataAlteracao;
+
+}
